09e282cf24bada824bcddb6f66b5421da3bbcab4
MaximeCode <contact.mbaude@gmail.com>
2026-06-10 17:58:40 +0200

    Client.ts moved into parent folder

--- frontend/src/api/strapi/historiques.ts
+++ frontend/src/api/strapi/historiques.ts
@@ -1,6 +1,5 @@
-import { fetchAPI } from "./Client";
+import { fetchAPI } from "../Client";
 import type { Historique } from "../../types/historiqueType";
-
 
 export async function getHistoriques(): Promise<Historique[]> {
   const { data } = await fetchAPI("/api/historiques?populate=*");
@@ -18,13 +17,11 @@ export async function getHistoriques(): Promise<Historique[]> {
       Titre: item.Titre,
       Description: item.Description,
       Date: item.Date,
-    }),
+    })
   );
 }
 
-export async function getOneHistorique(
-  documentId: string,
-): Promise<Historique> {
+export async function getOneHistorique(documentId: string): Promise<Historique> {
   const { data } = await fetchAPI(`/api/historiques/${documentId}?populate=*`);
   return {
     id: data.id,
